ae8885b406fa7123a8c76d74be5bf4d04bdf9c2f
mamingzhang <mamingzhang@shuzijiayuan.com>
2017-05-16 22:56:15 +0800

    InitProject

---
+++ app/src/main/java/com/example/horsege/ndkexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.horsege.ndkexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
